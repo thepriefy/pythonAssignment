73dfe050c130ad6fe7b4c3d9e86f51b1a4e7ad1b
Priefy <peachy-lovly-mom@hotmail.com>
2017-08-08 08:15:45 +0700

    add try-catch exception

--- firstPy.py
+++ firstPy.py
@@ -4,36 +4,42 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
+while True:
+    try:
+        print("Enter your INTEGER value of X and press Enter")
+        x = int(input('X: '))
 
-print("Enter your INTEGER value of X and press Enter")
-x = int(input('X: '))
+        print("Enter your INTEGER value of Y and press Enter")
+        y = int(input('Y: '))
 
-print("Enter your INTEGER value of Y and press Enter")
-y = int(input('Y: '))
+        # X+Y
+        print('X+Y: ', x + y)
 
-# X+Y
-print('X+Y: ', x + y)
+        # X-Y
+        print('X-Y: ', x - y)
 
-# X-Y
-print('X-Y: ', x - y)
+        # X*Y
+        print('X*Y: ', x * y)
 
-# X*Y
-print('X*Y: ', x * y)
+        # X^Y
+        print('X^Y: ', x ^ y)
 
-# X^Y
-print('X^Y: ', x ^ y)
+        # X!
+        print('X!: ', factorial(x))
 
-# X!
-print('X!: ', factorial(x))
+        # Y!
+        print('Y!: ', factorial(y))
 
-# Y!
-print('Y!: ', factorial(y))
+        # X/Y
+        print('X/Y: ', x / y)
 
-# X/Y
-print('X/Y: ', x / y)
+        # X//Y
+        print('X//Y: ', int(x / y))
 
-# X//Y
-print('X//Y: ', int(x / y))
+        # X%Y
+        print('X%Y: ', x % y)
 
-# X%Y
-print('X%Y: ', x % y)
+        print("========== End ==========")
+
+    except ValueError:
+        print('Please input an INTEGER...')
